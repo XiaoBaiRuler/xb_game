8dffe264742412691c9da25f474fcc960df38356
xiaobai <xiaobai.ruler@gmail.com>
2022-07-03 23:42:03 +0800

    Test01

--- Test/Test01.cc
+++ Test/Test01.cc
@@ -3,8 +3,10 @@
 #include <algorithm>
 #include <utility>
 #include <E:\GitStore\xb_game\DirectXMath\Inc\DirectXMath.h>
+#include <E:\GitStore\xb_game\DirectXMath\Inc\DirectXPackedVector.h>
 using namespace std;
 using namespace DirectX;
+using namespace DirectX::PackedVector;
 int main(){
     if (XMVerifyCPUSupport()){
         cout << "DirectXMath" << endl;
