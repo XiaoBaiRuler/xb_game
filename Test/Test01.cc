df95724f38be6246c6ab55b8150f6308976fc806
XiaoBaiRuler <xiaobai.ruler@gmail.com>
2022-07-04 18:12:36 +0800

    commit

---
+++ Test/Test01.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <windows.h>
+#include <algorithm>
+#include <utility>
+// #include <E:\GitStore\xb_game\DirectXMath\Inc\DirectXMath.h>
+#include <G:\game\xb_game\DirectXMath\Inc\DirectXMath.h>
+// #include <E:\GitStore\xb_game\DirectXMath\Inc\DirectXPackedVector.h>
+#include <G:\game\xb_game\DirectXMath\Inc\DirectXPackedVector.h>
+using namespace std;
+using namespace DirectX;
+using namespace DirectX::PackedVector;
+int main(){
+    if (XMVerifyCPUSupport()){
+        cout << "DirectXMath" << endl;
+    }
+    return EXIT_SUCCESS;
+}
