870644107fccb7b088bae7e1820dc1006b38c4a7
xiaobai <xiaobai.ruler@gmail.com>
2022-08-16 22:41:15 +0800

    vector

--- Test/Test01.cc
+++ Test/Test01.cc
@@ -2,10 +2,10 @@
 #include <windows.h>
 #include <algorithm>
 #include <utility>
-// #include <E:\GitStore\xb_game\DirectXMath\Inc\DirectXMath.h>
-#include <G:\game\xb_game\DirectXMath\Inc\DirectXMath.h>
-// #include <E:\GitStore\xb_game\DirectXMath\Inc\DirectXPackedVector.h>
-#include <G:\game\xb_game\DirectXMath\Inc\DirectXPackedVector.h>
+#include <E:\GitStore\xb_game\DirectXMath\Inc\DirectXMath.h>
+// #include <G:\game\xb_game\DirectXMath\Inc\DirectXMath.h>
+#include <E:\GitStore\xb_game\DirectXMath\Inc\DirectXPackedVector.h>
+// #include <G:\game\xb_game\DirectXMath\Inc\DirectXPackedVector.h>
 using namespace std;
 using namespace DirectX;
 using namespace DirectX::PackedVector;
