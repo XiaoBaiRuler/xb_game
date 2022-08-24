f9d229ac090dc400f938799bae07a63516341556
xiaobai <xiaobai.ruler@gmail.com>
2022-08-24 23:50:26 +0800

    XMVECTOR作为参数规则

--- Test/common.h
+++ Test/common.h
@@ -2,8 +2,8 @@
 #include <windows.h>
 #include <algorithm>
 #include <utility>
-#include "..//DirectXMath/Inc/DirectXMath.h"
-#include "..//DirectXMath/Inc/DirectXPackedVector.h"
+#include "../DirectXMath/Inc/DirectXMath.h"
+#include "../DirectXMath/Inc/DirectXPackedVector.h"
 using namespace std;
 using namespace DirectX;
 using namespace DirectX::PackedVector;
--- Test/vector.cc
+++ Test/vector.cc
@@ -1,5 +1,6 @@
 #include "common.h"
 int main(){
+    
     XMFLOAT2 a2(1.9, 2.1);
     // 加载
     XMVECTOR a = XMLoadFloat2(&a2);
@@ -21,5 +22,22 @@ int main(){
     // 存储
     XMFLOAT4 b;
     XMStoreFloat4(&b, a);
+
+
+    // 传递XMVECTOR参数的规则 - 必须初始化 -- 以XMVECTOR参数个数为准
+    // 1. 前面3个
+    FXMVECTOR a3(a);
+    // 2. 第4个
+    GXMVECTOR a4(a);
+    // 3. 第5、6个
+    HXMVECTOR a5(a);
+    // 4. 其他 
+    CXMVECTOR a7(a);
+
+    // 对于输入参数，何时使用SSE/SSE2寄存器
+    // 1. __fastcall 将前3个用寄存器
+    // 2. __vectorcall 将前6个用寄存器
+
+    // 添加编译选项编译 --mfma
     return EXIT_SUCCESS;
 }
